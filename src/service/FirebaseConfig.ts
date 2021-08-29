c5e9fb6aec10f8a7c3697400921763aeda00ffe5
tzikheng <tzikheng@gmail.com>
2021-08-29 23:36:14 +0800

    Adding FB config to git

---
+++ src/service/FirebaseConfig.ts
@@ -0,0 +1,10 @@
+const FirebaseConfig = {
+	apiKey: 'AIzaSyAwSSw3lno4sGwFQNwIb5Om1BSj3C7aw5M',
+	authDomain: 'mahjongapp-477c8.firebaseapp.com',
+	projectId: 'mahjongapp-477c8',
+	storageBucket: 'mahjongapp-477c8.appspot.com',
+	messagingSenderId: '458086526371',
+	appId: '1:458086526371:web:24268be9d25f313ec5fe16'
+};
+
+export default FirebaseConfig;
