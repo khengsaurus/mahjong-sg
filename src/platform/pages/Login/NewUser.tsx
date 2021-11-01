7ca6d7e53bee026d8faeac0c103c090a7f8838b3
tzikheng <tzikheng@gmail.com>
2021-11-01 22:54:20 +0800

    Remove formik

--- src/platform/pages/Login/NewUser.tsx
+++ src/platform/pages/Login/NewUser.tsx
@@ -1,95 +1,87 @@
-import { history } from "App";
-import "App.scss";
-import { Field, Form, Formik } from "formik";
-import FormField from "platform/components/FormComponents/FormField";
-import { HomeTheme } from "platform/style/MuiStyles";
-import { Main } from "platform/style/StyledComponents";
-import { StyledButton, Title } from "platform/style/StyledMui";
-import { useContext } from "react";
-import { Pages, Status } from "shared/enums";
-import { AppContext } from "shared/hooks";
-import { User } from "shared/models";
-import {
-  FBDeleteCurrentFBUser,
-  FBNewUser_EmailUser,
-  FBResolveUser_Email,
-} from "shared/service/fbUserFns";
+import TextField from '@material-ui/core/TextField';
+import Alert from '@material-ui/lab/Alert';
+import { history } from 'App';
+import 'App.scss';
+import { HomeTheme } from 'platform/style/MuiStyles';
+import { Main } from 'platform/style/StyledComponents';
+import { StyledButton, Title } from 'platform/style/StyledMui';
+import { useContext, useState } from 'react';
+import { Pages, Status } from 'shared/enums';
+import { AppContext } from 'shared/hooks';
+import { User } from 'shared/models';
+import { FBDeleteCurrentFBUser, FBNewUser_EmailUser, FBResolveUser_Email } from 'shared/service/fbUserFns';
 
 const NewUser = () => {
-  const { userEmail, login, logout, alert, setAlert } = useContext(AppContext);
+	const { userEmail, login, logout, alert, setAlert } = useContext(AppContext);
+	const [username, setUsername] = useState('');
 
-  function handleCancel() {
-    FBDeleteCurrentFBUser();
-    logout();
-    history.push(Pages.LOGIN);
-  }
+	function handleCancel() {
+		setAlert(null);
+		FBDeleteCurrentFBUser();
+		logout();
+		history.push(Pages.LOGIN);
+	}
 
-  function handleSubmit(values: IEmailUser, callback: () => void) {
-    FBNewUser_EmailUser(values)
-      .then((res) => {
-        if (res) {
-          setAlert({ status: Status.SUCCESS, msg: "Registered successfully" });
-          callback();
-        }
-      })
-      .catch((err) => {
-        setAlert({ status: Status.ERROR, msg: err.toString() });
-      });
-  }
+	function handleSubmit(values: IEmailUser, callback: () => void) {
+		FBNewUser_EmailUser(values)
+			.then(res => {
+				if (res) {
+					setAlert({ status: Status.SUCCESS, msg: 'Registered successfully' });
+					callback();
+				}
+			})
+			.catch(err => {
+				setAlert({ status: Status.ERROR, msg: err.toString() });
+			});
+	}
 
-  function successCallback() {
-    FBResolveUser_Email(userEmail)
-      .then((user: User) => {
-        login(user, true);
-        setTimeout(function () {
-          history.push(Pages.HOME);
-          setAlert(null);
-        }, 1000);
-      })
-      .catch((err) => {
-        if (err.message === "Username already taken") {
-          setAlert({ status: Status.ERROR, msg: err.message });
-        }
-      });
-  }
+	function successCallback() {
+		FBResolveUser_Email(userEmail)
+			.then((user: User) => {
+				login(user, true);
+				setTimeout(function () {
+					history.push(Pages.HOME);
+					setAlert(null);
+				}, 1000);
+			})
+			.catch(err => {
+				if (err.message === 'Username already taken') {
+					setAlert({ status: Status.ERROR, msg: err.message });
+				}
+			});
+	}
 
-  const markup = (
-    <HomeTheme>
-      <Main>
-        <Formik
-          initialValues={{ uN: "", email: userEmail }}
-          onSubmit={async (values) => {
-            handleSubmit(values, successCallback);
-          }}
-        >
-          {({ values }) => (
-            <>
-              <Title title={`Welcome! Choose a username`} />
-              <Form>
-                <Field name="uN" label="Username" component={FormField} />
-                <br></br>
-                <br></br>
-                <StyledButton
-                  label={`Submit`}
-                  autoFocus
-                  type="submit"
-                  disabled={values.uN.trim() === ""}
-                />
-                {alert?.status !== Status.SUCCESS && (
-                  <>
-                    <br></br>
-                    <StyledButton label="Cancel" onClick={handleCancel} />
-                  </>
-                )}
-              </Form>
-            </>
-          )}
-        </Formik>
-      </Main>
-    </HomeTheme>
-  );
-
-  return markup;
+	return (
+		<HomeTheme>
+			<Main>
+				<Title title={`Welcome! Choose a username`} />
+				<TextField
+					name="username"
+					label="Username"
+					onChange={e => {
+						setUsername(e.target.value);
+					}}
+				/>
+				<br></br>
+				<StyledButton
+					label={`Submit`}
+					autoFocus
+					type="submit"
+					disabled={username.trim() === ''}
+					onClick={() => {
+						handleSubmit({ email: userEmail, uN: username }, successCallback);
+					}}
+				/>
+				{alert?.status !== Status.SUCCESS ? <StyledButton label="Cancel" onClick={handleCancel} /> : null}
+				{alert && (
+					<>
+						<br></br>
+						<Alert severity={alert.status as 'success' | 'info' | 'warning' | 'error'}>{alert.msg}</Alert>
+					</>
+				)}
+			</Main>
+		</HomeTheme>
+	);
 };
 
 export default NewUser;
--- src/platform/pages/Login/index.tsx
+++ src/platform/pages/Login/index.tsx
@@ -1,166 +1,129 @@
-import Alert from "@material-ui/lab/Alert";
-import { history } from "App";
-import { Field, Form, Formik } from "formik";
-import FormField from "platform/components/FormComponents/FormField";
-import { HomeTheme } from "platform/style/MuiStyles";
-import { Centered, Main } from "platform/style/StyledComponents";
-import { StyledButton } from "platform/style/StyledMui";
-import { useContext, useState } from "react";
-import { Pages, Status } from "shared/enums";
-import { AppContext } from "shared/hooks";
-import {
-  FBAuthLogin_EmailPass,
-  FBAuthRegister_EmailPass,
-  FBResolveUser_Email,
-} from "shared/service/fbUserFns";
-import "./login.scss";
+import TextField from '@material-ui/core/TextField';
+import Alert from '@material-ui/lab/Alert';
+import { history } from 'App';
+import { HomeTheme } from 'platform/style/MuiStyles';
+import { Centered, Main } from 'platform/style/StyledComponents';
+import { StyledButton } from 'platform/style/StyledMui';
+import { useCallback, useContext, useState } from 'react';
+import { Pages, Status } from 'shared/enums';
+import { AppContext } from 'shared/hooks';
+import { FBAuthLogin_EmailPass, FBAuthRegister_EmailPass, FBResolveUser_Email } from 'shared/service/fbUserFns';
+import './login.scss';
 
 const Login = () => {
-  const [showRegister, setShowRegister] = useState(false);
-  const { login, setUserEmail, alert, setAlert } = useContext(AppContext);
+	const [showRegister, setShowRegister] = useState(false);
+	const { login, setUserEmail, alert, setAlert } = useContext(AppContext);
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-  function handleLogin(values: IEmailPass) {
-    FBAuthLogin_EmailPass(values)
-      .then((email) => {
-        if (email === values.email) {
-          setUserEmail(email);
-          FBResolveUser_Email(email)
-            .then((user) => {
-              setAlert(null);
-              if (user) {
-                login(user, false);
-                history.push(Pages.INDEX);
-              } else {
-                // User not registered, redirect to NewUser
-                history.push(Pages.NEWUSER);
-              }
-            })
-            .catch((err) => {
-              console.error(err);
-            });
-        } else {
-          // Auth login failed
-        }
-      })
-      .catch((err) => {
-        setAlert({ status: Status.ERROR, msg: err.toString() });
-      });
-  }
+	function clearForm() {
+		setEmail('');
+		setPassword('');
+	}
 
-  function handleRegister(values: IEmailPass, formCallback: () => void) {
-    FBAuthRegister_EmailPass(values)
-      .then((res) => {
-        if (res) {
-          formCallback();
-          handleLogin(values);
-        }
-      })
-      .catch((err) => {
-        setAlert({ status: Status.ERROR, msg: err.toString() });
-      });
-  }
+	const handleLogin = useCallback(
+		(values: IEmailPass, callback?: () => void) => {
+			FBAuthLogin_EmailPass(values)
+				.then(email => {
+					if (email === values.email) {
+						setUserEmail(email);
+						FBResolveUser_Email(email)
+							.then(user => {
+								setAlert(null);
+								if (user) {
+									callback();
+									login(user, false);
+									history.push(Pages.INDEX);
+								} else {
+									// User not registered, redirect to NewUser
+									history.push(Pages.NEWUSER);
+								}
+							})
+							.catch(err => {
+								console.error(err);
+							});
+					} else {
+						// Auth login failed
+					}
+				})
+				.catch(err => {
+					setAlert({ status: Status.ERROR, msg: err.toString() });
+				});
+		},
+		[login, setAlert, setUserEmail]
+	);
 
-  const loginMarkup: JSX.Element = (
-    <div>
-      <Formik
-        initialValues={{ email: "", password: "" }}
-        onSubmit={async (values) => {
-          handleLogin(values);
-        }}
-      >
-        {({ values }) => (
-          <Form>
-            <Field
-              name="email"
-              label="Email"
-              type="text"
-              component={FormField}
-            />
-            <br></br>
-            <br></br>
-            <Field
-              name="password"
-              label="Password"
-              type="password"
-              component={FormField}
-            />
-            <br></br>
-            <br></br>
-            <StyledButton
-              label={`Login`}
-              autoFocus
-              type="submit"
-              disabled={
-                values.email.trim() === "" || values.password.trim() === ""
-              }
-            />
-          </Form>
-        )}
-      </Formik>
-    </div>
-  );
+	const handleRegister = useCallback(
+		(values: IEmailPass, callback?: () => void) => {
+			FBAuthRegister_EmailPass(values)
+				.then(res => {
+					if (res) {
+						setAlert(null);
+						callback();
+						handleLogin(values);
+					}
+				})
+				.catch(err => {
+					setAlert({ status: Status.ERROR, msg: err.toString() });
+				});
+		},
+		[handleLogin, setAlert]
+	);
 
-  const registerMarkup = (
-    <Formik
-      initialValues={{ email: "", password: "" }}
-      onSubmit={async (values, { resetForm }) => {
-        handleRegister(values, resetForm);
-      }}
-    >
-      {({ values }) => (
-        <Form>
-          <Field name="email" label="Email" component={FormField} />
-          <br></br>
-          <br></br>
-          <Field
-            name="password"
-            label="Password"
-            type="password"
-            component={FormField}
-          />
-          <br></br>
-          <br></br>
-          <StyledButton
-            label={`Register`}
-            autoFocus
-            type="submit"
-            disabled={
-              values.email.trim() === "" || values.password.trim() === ""
-            }
-          />
-        </Form>
-      )}
-    </Formik>
-  );
-
-  return (
-    <HomeTheme>
-      <Main>
-        <Centered>
-          {showRegister ? registerMarkup : loginMarkup}
-          <StyledButton
-            label={showRegister ? `Back to login` : `Register now`}
-            onClick={() => {
-              setAlert(null);
-              setShowRegister(!showRegister);
-            }}
-          />
-          {alert && (
-            <>
-              <br></br>
-              <Alert
-                severity={
-                  alert.status as "success" | "info" | "warning" | "error"
-                }
-              >
-                {alert.msg}
-              </Alert>
-            </>
-          )}
-        </Centered>
-      </Main>
-    </HomeTheme>
-  );
+	return (
+		<HomeTheme>
+			<Main>
+				<Centered>
+					<TextField
+						key="email"
+						label="Email"
+						type="text"
+						value={email}
+						onChange={e => {
+							setEmail(e.target.value);
+						}}
+					/>
+					<br></br>
+					<TextField
+						key="password"
+						label="Password"
+						type="password"
+						value={password}
+						onChange={e => {
+							setPassword(e.target.value);
+						}}
+					/>
+					<br></br>
+					<StyledButton
+						label={showRegister ? `Register` : `Login`}
+						type="submit"
+						autoFocus
+						disabled={email.trim() === '' || password.trim() === ''}
+						onClick={() =>
+							showRegister
+								? handleRegister({ email, password }, clearForm)
+								: handleLogin({ email, password }, clearForm)
+						}
+					/>
+					<StyledButton
+						label={showRegister ? `Back to login` : `Register now`}
+						onClick={() => {
+							setAlert(null);
+							setShowRegister(!showRegister);
+						}}
+					/>
+					{alert && (
+						<>
+							<br></br>
+							<Alert severity={alert.status as 'success' | 'info' | 'warning' | 'error'}>
+								{alert.msg}
+							</Alert>
+						</>
+					)}
+				</Centered>
+			</Main>
+		</HomeTheme>
+	);
 };
 
 export default Login;
