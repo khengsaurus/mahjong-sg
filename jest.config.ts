72581eae51773dbbb23d3c2b915b0e99e34275be
tzikheng <tzikheng@gmail.com>
2021-11-11 23:44:27 +0800

    config for jest

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+const config = {
+  // testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  testMatch: ['**/__tests__/**handLogic.test.tsx'],
+  clearMocks: true,
+  collectCoverage: false,
+  coverageDirectory: 'coverage',
+  moduleDirectories: ['node_modules', 'src'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  moduleNameMapper: {
+    '\\.(csslessscss)$': '<rootDir>/__mocks__/styleMock.js'
+  },
+  setupFiles: ['<rootDir>/jest.init.js'],
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setupTests.js'],
+  testPathIgnorePatterns: ['node_modules'],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+    '^.+\\.(js|jsx|mjs)': 'babel-jest',
+    '^.+\\.(jpg|jpeg|png|gif|eot|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|)': '<rootDir>/__mocks__/imageMock.js',
+    '^.+\\.(css|less|scss)': '<rootDir>/__mocks__/styleMock.js'
+  }
+};
+
+export default config
---
+++ jest.init.js
@@ -0,0 +1,5 @@
+const React = require('react');
+const { configure } = require('enzyme');
+const Adapter = require('@wojtekmaj/enzyme-adapter-react-17');
+
+configure({ adapter: new Adapter() });
