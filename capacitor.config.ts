462b1a49b52155f954bb58b8956bfb00aa3cb9ea
tzikheng <tzikheng@gmail.com>
2022-01-03 15:32:28 +0800

    edit ionic cap

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-	appId: 'com.tk.mahjongApp',
-	appName: 'Mahjong',
+	appId: 'com.tk.mahjongSg',
+	appName: 'Mahjong SG',
 	webDir: 'build',
 	bundledWebRuntime: false,
 	plugins: {
